2bd0aeafdb641b5931157160c4ee745683d4cd4d
Imesh Gunaratne <imesh@wso2.com>
2016-06-03 00:14:04 +0530

    Adding deploy.sh, undeploy.sh to wso2-shared-dbs

---
+++ wso2-shared-dbs/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+#
+# Copyright 2016 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+# ------------------------------------------------------------------------
+
+set -e
+self_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+marathon_endpoint="http://m1.dcos:8080/v2"
+product_name="wso2esb"
+product_version="4.9.0"
+
+source "${self_path}/../scripts/base.sh"
+
+deploy ${marathon_endpoint} ${self_path}/mysql-govdb.json
+deploy ${marathon_endpoint} ${self_path}/mysql-userdb.json
